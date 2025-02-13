598905361b568346bd836a97f3f7b9c7a1029fa7
Juha Jeronen <juha.m.jeronen@gmail.com>
2025-02-13 16:44:55 +0200

    remove outdated comment

--- raven/preprocess.py
+++ raven/preprocess.py
@@ -13,11 +13,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# # Output log messages only from this module. Handy for debugging.
-# # https://stackoverflow.com/questions/17275334/what-is-a-correct-way-to-filter-different-loggers-using-python-logging
-# for handler in logging.root.handlers:
-#     handler.addFilter(logging.Filter(__name__))
-
 import argparse
 import collections
 import functools
