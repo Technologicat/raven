2f542d7dc5e54a9a7a819ed6c25c81470829f5a9
Juha Jeronen <juha.m.jeronen@gmail.com>
2025-06-26 02:14:09 +0300

    improve comments

--- raven/server/modules/avatarutil.py
+++ raven/server/modules/avatarutil.py
@@ -253,15 +253,15 @@ supported_cels = [
     # These are automatically activated by the live animator when one of the trigger emotion states is entered.
     # Rendered after the character. Applied after posing, but before upscaling or postprocessing.
     "fx_angervein1", "fx_angervein2",  # hovering stylized forehead veins. Cycle, while fading out.
-    "fx_sweatdrop1", "fx_sweatdrop2", "fx_sweatdrop3",  # hovering sweatdrop. If have just cel 1: fade out. If all cels: flash in sequence, while fading out.
-    "fx_heart1", "fx_heart2", "fx_heart3",  # hovering heart(s). If have just cel 1: fade out. If all cels: flash in sequence, while fading out.
+    "fx_sweatdrop1", "fx_sweatdrop2", "fx_sweatdrop3",  # hovering sweatdrop. Show in sequence, while fading out. Omit any missing cels.
+    "fx_heart1", "fx_heart2", "fx_heart3",  # hovering heart(s). Show in sequence, while fading out. Omit any missing cels.
     "fx_blackcloud1", "fx_blackcloud2",  # hovering black cloud representing frustration. Cycle, while fading out.
     "fx_flowers1", "fx_flowers2",  # hovering flowers. Cycle, while fading out.
     "fx_shock1",  # shock lines. Fade out.
-    "fx_notice1", "fx_notice2",  # notice lines (or surprise lines). Quickly flash cel1, cel2, cel1, cel2, then off.
-    "fx_beaming1", "fx_beaming2",  # happy lines (beaming joy). Quickly flash cel1, cel2, then off.
-    "fx_question1", "fx_question2", "fx_question3",  # Question mark(s), confusion. Quickly flash cel1, cel2, cel3, then off.
-    "fx_exclaim1", "fx_exclaim2", "fx_exclaim3",  # Exclamation mark(s), realization. Quickly flash cel1, cel2, cel3, then off.
+    "fx_notice1", "fx_notice2",  # notice lines (or surprise lines). Show cel1, cel2, cel1, cel2, then off.
+    "fx_beaming1", "fx_beaming2",  # happy lines (beaming joy). Show cel1, cel2, then off.
+    "fx_question1", "fx_question2", "fx_question3",  # Question mark(s), confusion. Show cel1, cel2, cel3, then off.
+    "fx_exclaim1", "fx_exclaim2", "fx_exclaim3",  # Exclamation mark(s), realization. Show cel1, cel2, cel3, then off.
 ]
 
 def scan_addon_cels(image_file_name: str) -> Dict[str, str]:
