d4cd323a3fb099f4da93805d0101eb61f0c083c9
Juha Jeronen <juha.m.jeronen@gmail.com>
2025-09-23 15:44:03 +0300

    add an raven-avatar-settings-editor only server config

---
+++ raven/server/config_avatar_only.py
@@ -0,0 +1,23 @@
+"""Avatar settings editor only alternative config. Necessary modules only.
+
+For running just `raven-avatar-settings-editor`.
+
+To use this, start the server with:
+
+  raven-server --config raven.server.config_avatar_only
+"""
+
+import torch
+
+from .config import *  # Use the default config as a base.  # noqa: F401, F403
+
+# We override just this.
+enabled_modules = {
+    "avatar": {"device_string": "cuda:0",
+               "dtype": torch.float16},
+    "classify": {"device_string": "cuda:0",
+                 "dtype": torch.float16},
+    "imagefx": {"device_string": "cuda:0",
+                "dtype": torch.float16},
+    "tts": {"device_string": "cuda:0"},
+}
