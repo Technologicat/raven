cd5800fd3a1b6a985ccdf7a0a9c04316c449299f
Juha Jeronen <juha.m.jeronen@gmail.com>
2025-09-25 23:38:26 +0300

    change low VRAM config

--- raven/server/config_lowvram.py
+++ raven/server/config_lowvram.py
@@ -1,4 +1,4 @@
-"""Low-VRAM (8GB) alternative config that runs all Raven modules on CPU, leaving all VRAM available for the LLM server.
+"""Low-VRAM (8GB) alternative config that runs most Raven modules on CPU, leaving most VRAM available for the LLM server.
 
 To use this, start the server with:
 
@@ -9,14 +9,14 @@ import torch
 
 from .config import *  # Use the default config as a base.  # noqa: F401, F403
 
-# We override just this, disabling the avatar, and running all other modules on CPU.
+# We override just this, running everything except the avatar on CPU.
 #
-# All modules that require heavy compute will be horribly slow - but the point is, a 7B/8B LLM needs
-# about 8GB VRAM by itself, so when running with low VRAM, we can't afford to put anything else on the GPU.
+# All modules that require heavy compute will be horribly slow - but the point is, even Qwen3 2507 4B
+# at 4bit and decent context size needs ~6GB VRAM by itself, so when running with low VRAM, we can't afford
+# to put anything else on the GPU, except possibly the avatar.
 enabled_modules = {
-    # # no avatar when running with low VRAM
-    # "avatar": {"device_string": "cuda:0",
-    #            "dtype": torch.float16},
+    "avatar": {"device_string": "cuda:0",
+               "dtype": torch.float16},
     "classify": {"device_string": "cpu",
                  "dtype": torch.float32},
     "embeddings": {"device_string": "cpu",
