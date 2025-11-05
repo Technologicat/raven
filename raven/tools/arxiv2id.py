f6884ae86500c23448c43eca86c7e66f3e22ef32
Juha Jeronen <juha.m.jeronen@gmail.com>
2025-11-05 10:40:43 +0200

    arxiv2id: include only most recent version of each paper discovered

--- raven/tools/arxiv2id.py
+++ raven/tools/arxiv2id.py
@@ -9,14 +9,14 @@ to create a BibTeX bibliography of those papers (by auto-downloading metadata fr
 from .. import __version__
 
 import argparse
-import operator
 import os
 import pathlib
 import re
+from typing import List
 
 from unpythonic import uniqify
 
-def list_subfolders(path):
+def list_subfolders(path: str) -> List[str]:
     # blacklist = []  # ["stuff"]  # don't descend into these directories, at any level. Directories with names beginning with "00_" are automatically ignored.
     paths = []
     for root, dirs, files in os.walk(path):
@@ -35,7 +35,7 @@ def list_pdf_files(path):
     return list(sorted(filename for filename in os.listdir(path) if filename.lower().endswith(".pdf")))
 
 arxiv_identifier = re.compile(r"\b(\d\d\d\d\.\d\d\d\d\d)(v\d+)?\b")
-def get_arxiv_identifier(path):
+def get_arxiv_identifier(path: str) -> str:
     filename = os.path.basename(path)
     matches = re.findall(arxiv_identifier, filename)
     matches = [''.join(x) for x in matches]  # [("yymm.xxxxx", "v2"), ...] -> ["yymm.xxxxxv2", ...]
@@ -44,8 +44,15 @@ def get_arxiv_identifier(path):
         return matches[0]
     return None
 
-def main():
-    parser = argparse.ArgumentParser(description="""List identifiers of arXiv papers in the specified directory. The papers are assumed to be PDF files with the arXiv identifier (yymm.xxxxx) somewhere in the filename. Only unique identifiers are returned (even if duplicated under different filenames, as long as the identifier is the same).""",
+def split_arxiv_identifier(raw_identifier: str) -> int:
+    splitted = raw_identifier.split("v")
+    if len(splitted) == 1:  # no version
+        return raw_identifier.strip(), 1
+    base, version = splitted
+    return base.strip(), int(version.strip())
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="""List identifiers of arXiv papers in the specified directory. The papers are assumed to be PDF files with the arXiv identifier (yymm.xxxxx) somewhere in the filename. Only unique identifiers are returned (even if duplicated under different filenames, as long as the identifier is the same). To avoid duplicates, in case of multiple versions of the same paper (yymm.xxxxxvN), only the most recent version is returned.""",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("-i", "--input-dir", dest="input_dir", default=None, type=str, metavar="input_dir", help="Input directory containing arXiv PDF file(s).")
     parser.add_argument('-v', '--version', action='version', version=('%(prog)s ' + __version__))
@@ -55,18 +62,26 @@ def main():
     if opts.input_dir is None:
         opts.input_dir = "."
 
-    arxiv_pdf_files = []
+    arxiv_pdf_files = {}
     paths = list_subfolders(opts.input_dir)
     for path in sorted(paths):
         pdf_files = list_pdf_files(path)
         if not pdf_files:
             continue
-        arxiv_pdf_files_in_this_dir = [(arxiv_id, p) for p in pdf_files if (arxiv_id := get_arxiv_identifier(p))]
-        arxiv_pdf_files.extend(arxiv_pdf_files_in_this_dir)
+        arxiv_pdf_files_in_this_dir = [(raw_id, p) for p in pdf_files if (raw_id := get_arxiv_identifier(p))]
+        base_ids_and_versions = [split_arxiv_identifier(raw_id) for raw_id, p in arxiv_pdf_files_in_this_dir]
 
-    arxiv_pdf_files = list(uniqify(sorted(arxiv_pdf_files),
-                                   key=operator.itemgetter(0)))
-    for identifier, filename in arxiv_pdf_files:
+        # Pick the latest version for each identifier discovered so far
+        for (raw_id, p), (base_id, version) in zip(arxiv_pdf_files_in_this_dir, base_ids_and_versions):
+            if (base_id not in arxiv_pdf_files):
+                arxiv_pdf_files[base_id] = (raw_id, p, version)
+            else:
+                recorded_raw_id, recorded_p, recorded_version = arxiv_pdf_files[base_id]
+                if version > recorded_version:
+                    arxiv_pdf_files[base_id] = (raw_id, p, version)
+
+    arxiv_pdf_files = list(sorted(arxiv_pdf_files.values()))
+    for identifier, filename, version in arxiv_pdf_files:
         if opts.verbose:
             print(identifier, filename)
         else:
